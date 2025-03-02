144f479a1eb5f0ef22e2428011d0fc7d7646b5ea
wines-ev <winesevander@gmail.com>
2025-03-02 23:27:31 +0800

    final

--- account-management.php
+++ account-management.php
@@ -17,96 +17,181 @@
 	<div class="container-fluid mx-0 px-0">
 		<div class="accounts-hero d-flex align-items-start">
 			<div>
-				<?php include("tabs.php"); ?>
+				<div id="sidenav" class="sidenav position-relative d-flex flex-column gap-4 bg-blue z-1 overflow-hidden px-4" style="width: 20rem; height: 100vh;">
+					<div class="d-flex align-items-center justify-content-between w-100 mb-5">
+						<div class="d-flex align-items-center" style="height: 6rem;">
+							<img class="au_logo" src="./assets/img/au_logo.png" alt="" style="width: 4rem; height: 4rem;">
+							<p id="sidenav_title" class="fs-2 ms-2 mb-0 text-light">TSMS</p>
+						</div>
+						<span id="close-nav-icon" class="text-light fs-2" style="cursor:pointer" onclick="closeNav()">&#9776;</span>
+					</div>
+
+
+
+					<div class="d-flex align-items-center mb-4">
+						<i class="fa-solid fa-chart-simple fs-1 text-light text-center" style="width: 5rem;"></i>
+						<a class="navtab-text text-light fs-4" href="#">Dashboard</a>
+					</div>
+					
+					<div class="d-flex align-items-center mb-4">
+						<i class="fa-solid fa-users fs-1 text-light text-center" style="width: 5rem;"></i>
+						<a class="navtab-text text-light fs-4" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+							Accounts
+						</a>
+					</div>
+					
+					<div class="collapse" id="collapseExample">
+						<div class="ms-3 d-flex flex-column">
+							<a class="text-light" href="#">All accounts</a>
+							<a class="text-light" href="#">Administrator</a>
+							<a class="text-light" href="#">Technical</a>
+							<a class="text-light" href="#">Staff</a>
+							<a class="text-light" href = "create-account.php">Create new account</a>
+						</div>
+					</div>
+					<div class="position-absolute d-flex align-items-center" style="bottom: 2rem;">
+						<i class="fa-solid fa-door-open fs-1 text-light text-center" style="width: 4rem;"></i>
+						<a class="text-light fs-4" href = "logout.php">Logout</a>
+					</div>
+				</div>
 			</div>
 			
 			<div class="accounts-con">
-				<div class="account-header">
+				<div class="account-header d-flex align-items-center" style="height: 6rem;">
 					
+				
+					<span id="open-nav-icon" class="ms-5 fs-2" style="font-size:2rem; cursor:pointer;" onclick="openNav()">&#9776;</span>
+					<div class="ms-5">
+						<?php
+							if(isset($_SESSION['username']))
+							{
+								echo "<p class='mb-0 fs-1'>Welcome, " . $_SESSION['username'] . "</p>";
+								echo "<p class='mb-0 fs-4'>Account Type:  " . $_SESSION['usertype'] . "</p>";
+							}
+							else
+							{
+								header("location: login.php");
+							}
+						?>
+					</div>
+					
+				
 				</div>
-				<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
-				<?php
-					if(isset($_SESSION['username']))
-					{
-						echo "<h1>Welcome, " . $_SESSION['username'] . "</h1>";
-						echo "<h4>Account Type:  " . $_SESSION['usertype'] . "</h4>";
-					}
-					else
-					{
-						header("location: login.php");
-					}
-				?>
 
-				<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "POST">
-						<a href = "create-account.php">Create new account</a>
-						<a href = "logout.php">Logout</a><br></br>
-						<br>Search: <input type="text" name="txtsearch">
-						<input type="submit" name="btnsearch" value="Search">
-				</form>
-				<?php
-					include("config.php");
+				<hr class="mt-0">
+				
+				<div class="d-flex justify-content-between mx-5">
+					<p class="fs-4 mb-0">Accounts / All accounts</p>
+					<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "POST">
+						<div class="d-flex align-items-center">
+							<input class="fs-5 ps-2" type="text" name="txtsearch" placeholder="Seach here...">
+							<input class="fs-5 px-2" type="submit" name="btnsearch" value="Search">
+						</div>
+					</form>
+				</div>
+				
+				<div class="accout-table-con mx-5 fs-5">
+					<?php
+						include("config.php");
 
-					if(!isset($_POST["txtsearch"])) {
-						$sql = "SELECT * FROM tblaccounts";
+						if(!isset($_POST["txtsearch"])) {
+							$sql = "SELECT * FROM tblaccounts";
 
-						if($stmt = mysqli_prepare($link, $sql)) {
-							if (mysqli_stmt_execute($stmt)) {
-								$result = mysqli_stmt_get_result($stmt);
-								buildtable($result);
+							if($stmt = mysqli_prepare($link, $sql)) {
+								if (mysqli_stmt_execute($stmt)) {
+									$result = mysqli_stmt_get_result($stmt);
+									buildtable($result);
+								}
 							}
 						}
-					}
-					else {
-						$sql = "SELECT * FROM tblaccounts WHERE username LIKE ? OR usertype LIKE ? ORDER BY username";
+						else {
+							$sql = "SELECT * FROM tblaccounts WHERE username LIKE ? OR usertype LIKE ? ORDER BY username";
 
-						if($stmt = mysqli_prepare($link, $sql)) {
-							$text_value = "%" . $_POST["txtsearch"] . "%";
-							
-							
-							mysqli_stmt_bind_param($stmt, "ss", $text_value, $text_value);
-							if (mysqli_stmt_execute($stmt)) {
-								$result = mysqli_stmt_get_result($stmt);
-								buildtable($result);
+							if($stmt = mysqli_prepare($link, $sql)) {
+								$text_value = "%" . $_POST["txtsearch"] . "%";
+								
+								
+								mysqli_stmt_bind_param($stmt, "ss", $text_value, $text_value);
+								if (mysqli_stmt_execute($stmt)) {
+									$result = mysqli_stmt_get_result($stmt);
+									buildtable($result);
+								}
 							}
 						}
-					}
-					function buildtable($result)
-					{
-						if(mysqli_num_rows($result) > 0)
-						{
-							echo "<table>";
-							echo "<tr>";
-							echo "<th>Username</th><th>Usertype</th><th>Status</th><th>Created By</th>
-							<th>Date Created</th><th>Action</th>";
-							echo "</tr>";
-							echo "<br>";
 
-						while($row = mysqli_fetch_array($result))
-						{
-							echo "<tr>";
-							echo "<td>" . $row['username'] . "</td>";
-							echo "<td>" . $row['usertype'] . "</td>";
-							echo "<td>" . $row['status'] . "</td>";
-							echo "<td>" . $row['createdby'] . "</td>";
-							echo "<td>" . $row['datecreated'] . "</td>";
-							echo "<td>";
-							echo "<a href = 'update-account.php?username=" . $row['username'] . "'>Update</a>";
-							echo "<a href = 'delete0-account.php?username=" . $row['username'] . "'>Delete</a>";
-							echo "</td>";
-							echo "</tr>";
+						function buildtable($result) {
+							if(mysqli_num_rows($result) > 0) {
+								echo "<table id='account-table'>";
+								echo "<tr>";
+								echo "<th>Username</th><th>Usertype</th><th>Status</th><th>Created By</th>
+								<th>Date Created</th><th>Action</th>";
+								echo "</tr>";
+								echo "<br>";
+
+							while($row = mysqli_fetch_array($result)) {
+								echo "<tr id='data-row' >";
+								echo "<td>" . $row['username'] . "</td>";
+								echo "<td>" . $row['usertype'] . "</td>";
+								echo "<td>" . $row['status'] . "</td>";
+								echo "<td>" . $row['createdby'] . "</td>";
+								echo "<td>" . $row['datecreated'] . "</td>";
+								echo "<td>";
+								echo "<a href = 'update-account.php?username=" . $row['username'] . "'>Update</a> ";
+								echo "<a href = 'delete0-account.php?username=" . $row['username'] . "'>Delete</a>";
+								echo "</td>";
+								echo "</tr>";
+							}
+								echo "</table>";
+							}
+							else {
+								echo "No rercord/s found.";
+							}
 						}
-							echo "</table>";
-					}
-					else
-					{
-						echo "No rercord/s found.";
-					}
-				}
+					?>
+				</div>
 
-				?>
+				
 			</div>
 		</div>		
 	</div>
 	
 </body>
+
+<script>
+	const open_nav_icon = document.getElementById("open-nav-icon");
+	const close_nav_icon = document.getElementById("close-nav-icon");
+	const sidenav_title = document.getElementById("sidenav_title");
+	const navtab_texts = document.getElementsByClassName("navtab-text");
+
+	function openNav() {
+		document.getElementById("sidenav").style.width = "20rem";
+		open_nav_icon.style.display = "none";
+		close_nav_icon.style.display = "block";
+		sidenav_title.style.display = "block";
+
+		for(let text of navtab_texts) {
+			text.style.display = "block";
+		}
+	}
+
+	function closeNav() {
+		document.getElementById("sidenav").style.width = "7rem";
+		open_nav_icon.style.display = "block";
+		close_nav_icon.style.display = "none";
+		sidenav_title.style.display = "none";
+
+		for(let text of navtab_texts) {
+			text.style.display = "none";
+		}
+	}
+
+	const rows = document.getElementById("account-table").childNodes[0].childNodes;
+	
+	for (var i = 0; i < rows.length; i++) {
+		if (i%2 == 0) {
+			rows[i].classList.add("bg-blue-50");
+		}
+	}
+	
+</script>
 </html>
--- tabs.php
+++ tabs.php
@@ -1,60 +1,17 @@
 <style>
-    .sidenav {
-        height: 100%;
-        width: 6rem;
-        z-index: 1;
-        overflow-x: hidden;
-        padding-top: 60px;
-    }
 
-    .sidenav a {
-        padding: 8px 8px 8px 32px;
-        text-decoration: none;
-        color: #818181;
-        display: block;
-    }
 
-    .sidenav a:hover {
-        color: #f1f1f1;
-    }
 
-    .sidenav .closebtn {
-        font-size: 36px;
-        margin-left: 50px;
-    }
 
-    .sidenav .au_logo {
-        width: 4rem;
-        height: 4rem;
-    }
 
-    @media screen and (max-height: 450px) {
-        .sidenav {padding-top: 15px;}
-        .sidenav a {font-size: 18px;}
-    }
+
+
+
+    
 </style>
 
-<div id="sidenav" class="sidenav d-flex flex-column fs-4 bg-blue">
-    <div class="d-flex">
-        <div class="d-flex">
-            <img class="au_logo" src="./assets/img/au_logo.png" alt="">
-            <p class="fs-2">TSMS</p>
-        </div>
-        <span style="font-size:30px;cursor:pointer" onclick="closeNav()">&#9776;</span>
-    </div>
 
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
-</div>
 
 <script>
-function openNav() {
-  document.getElementById("sidenav").style.width = "25rem";
-}
 
-function closeNav() {
-  document.getElementById("sidenav").style.width = "6rem";
-}
 </script>

eeb2a3b8840598885d2176f0cd90af15aa85dfd5
wines-ev <winesevander@gmail.com>
2025-03-02 19:48:16 +0800

    account css added

--- account-management.php
+++ account-management.php
@@ -1,85 +1,112 @@
-<html>
-<title>Accounts Management Page - AU Technical Support Management System</title>
+<?php	
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Accounts Management Page - AU Technical Support Management System</title>
+	<link rel="stylesheet" href="./css/style.css">
+	<link rel="stylesheet" href="./plugins/bs/bootstrap.min.css">
+	<script src="./plugins/bs/bootstrap.min.js"></script>
+	<script src="https://kit.fontawesome.com/acb62c1ffe.js" crossorigin="anonymous"></script>
+</head>
 <body>
-	<?php
-		session_start();
-		if(isset($_SESSION['username']))
-		{
-			echo "<h1>Welcome, " . $_SESSION['username'] . "</h1>";
-			echo "<h4>Account Type:  " . $_SESSION['usertype'] . "</h4>";
-		}
-		else
-		{
-			header("location: login.php");
-		}
-	?>
-	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "POST">
-		<a href = "create-account.php">Create new account</a>
-		<a href = "logout.php">Logout</a><br></br>
-		<br>Search: <input type="text" name="txtsearch">
-		<input type="submit" name="btnsearch" value="Search">
-	</form>
+	<div class="container-fluid mx-0 px-0">
+		<div class="accounts-hero d-flex align-items-start">
+			<div>
+				<?php include("tabs.php"); ?>
+			</div>
+			
+			<div class="accounts-con">
+				<div class="account-header">
+					
+				</div>
+				<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+				<?php
+					if(isset($_SESSION['username']))
+					{
+						echo "<h1>Welcome, " . $_SESSION['username'] . "</h1>";
+						echo "<h4>Account Type:  " . $_SESSION['usertype'] . "</h4>";
+					}
+					else
+					{
+						header("location: login.php");
+					}
+				?>
+
+				<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "POST">
+						<a href = "create-account.php">Create new account</a>
+						<a href = "logout.php">Logout</a><br></br>
+						<br>Search: <input type="text" name="txtsearch">
+						<input type="submit" name="btnsearch" value="Search">
+				</form>
+				<?php
+					include("config.php");
+
+					if(!isset($_POST["txtsearch"])) {
+						$sql = "SELECT * FROM tblaccounts";
+
+						if($stmt = mysqli_prepare($link, $sql)) {
+							if (mysqli_stmt_execute($stmt)) {
+								$result = mysqli_stmt_get_result($stmt);
+								buildtable($result);
+							}
+						}
+					}
+					else {
+						$sql = "SELECT * FROM tblaccounts WHERE username LIKE ? OR usertype LIKE ? ORDER BY username";
+
+						if($stmt = mysqli_prepare($link, $sql)) {
+							$text_value = "%" . $_POST["txtsearch"] . "%";
+							
+							
+							mysqli_stmt_bind_param($stmt, "ss", $text_value, $text_value);
+							if (mysqli_stmt_execute($stmt)) {
+								$result = mysqli_stmt_get_result($stmt);
+								buildtable($result);
+							}
+						}
+					}
+					function buildtable($result)
+					{
+						if(mysqli_num_rows($result) > 0)
+						{
+							echo "<table>";
+							echo "<tr>";
+							echo "<th>Username</th><th>Usertype</th><th>Status</th><th>Created By</th>
+							<th>Date Created</th><th>Action</th>";
+							echo "</tr>";
+							echo "<br>";
+
+						while($row = mysqli_fetch_array($result))
+						{
+							echo "<tr>";
+							echo "<td>" . $row['username'] . "</td>";
+							echo "<td>" . $row['usertype'] . "</td>";
+							echo "<td>" . $row['status'] . "</td>";
+							echo "<td>" . $row['createdby'] . "</td>";
+							echo "<td>" . $row['datecreated'] . "</td>";
+							echo "<td>";
+							echo "<a href = 'update-account.php?username=" . $row['username'] . "'>Update</a>";
+							echo "<a href = 'delete0-account.php?username=" . $row['username'] . "'>Delete</a>";
+							echo "</td>";
+							echo "</tr>";
+						}
+							echo "</table>";
+					}
+					else
+					{
+						echo "No rercord/s found.";
+					}
+				}
+
+				?>
+			</div>
+		</div>		
+	</div>
+	
 </body>
 </html>
-
-<?php
-	include("config.php");
-
-	if(!isset($_POST["txtsearch"])) {
-		$sql = "SELECT * FROM tblaccounts";
-
-		if($stmt = mysqli_prepare($link, $sql)) {
-			if (mysqli_stmt_execute($stmt)) {
-				$result = mysqli_stmt_get_result($stmt);
-				buildtable($result);
-			}
-		}
-	}
-	else {
-		$sql = "SELECT * FROM tblaccounts WHERE username LIKE ? OR usertype LIKE ? ORDER BY username";
-
-		if($stmt = mysqli_prepare($link, $sql)) {
-			$text_value = "%" . $_POST["txtsearch"] . "%";
-			
-			
-			mysqli_stmt_bind_param($stmt, "ss", $text_value, $text_value);
-			if (mysqli_stmt_execute($stmt)) {
-				$result = mysqli_stmt_get_result($stmt);
-				buildtable($result);
-			}
-		}
-	}
-	function buildtable($result)
-	{
-		if(mysqli_num_rows($result) > 0)
-		{
-			echo "<table>";
-			echo "<tr>";
-			echo "<th>Username</th><th>Usertype</th><th>Status</th><th>Created By</th>
-			<th>Date Created</th><th>Action</th>";
-			echo "</tr>";
-			echo "<br>";
-
-		while($row = mysqli_fetch_array($result))
-		{
-			echo "<tr>";
-			echo "<td>" . $row['username'] . "</td>";
-			echo "<td>" . $row['usertype'] . "</td>";
-			echo "<td>" . $row['status'] . "</td>";
-			echo "<td>" . $row['createdby'] . "</td>";
-			echo "<td>" . $row['datecreated'] . "</td>";
-			echo "<td>";
-			echo "<a href = 'update-account.php?username=" . $row['username'] . "'>Update</a>";
-			echo "<a href = 'delete0-account.php?username=" . $row['username'] . "'>Delete</a>";
-			echo "</td>";
-			echo "</tr>";
-		}
-			echo "</table>";
-	}
-	else
-	{
-		echo "No rercord/s found.";
-	}
-}
-require_once "config.php";
-?>
--- login.php
+++ login.php
@@ -40,55 +40,88 @@ if ($stmt = mysqli_prepare($link, $sql)) {
 	<link rel="stylesheet" href="./css/style.css">
 	<link rel="stylesheet" href="./plugins/bs/bootstrap.min.css">
 	<script src="./plugins/bs/bootstrap.min.js"></script>
+	<script src="https://kit.fontawesome.com/acb62c1ffe.js" crossorigin="anonymous"></script>
 </head>
-<body>
-	<div class="hero row justify-content-center">
-		<div class="col-3 px-0 login-con">
-			<form class="login-form d-flex flex-column px-5 py-4 bg-white rounded-start" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "POST">
-				<p class="fs-1 mb-4 text-center">Welcome back</p>
-				<label for="txtusername" class="fs-4">Username</label>
-				<input type = "text" class="form-control p-2 ps-3 fs-4 mb-4" name = "txtusername" id = "txtusername">
+<body class="login-body ">
+	<div class="mask"></div>
 
-				<div class="d-flex 	justify-content-between fs-4">
-					<label for="txtusername">Password</label>
-					<a href="#">Forgot Password?</a>
+	<div class="container">
+		<div class="hero row justify-content-center bg-transparent">
+			<div class="login-con col-10 col-md-7 col-lg-4 px-0 bg-white">
+				<p class="mb-4 fs-1 text-center">Welcome Back!</p>
+				<form class="login-form d-flex flex-column px-5 py-4" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method = "POST">
+					
+					<label for="txtusername" class="fs-4">Username</label>
+					<input type = "text" class="form-control p-2 ps-3 fs-4 mb-4" placeholder="Enter username" name = "txtusername" id = "txtusername">
+
+					<label for="txtusername" class="fs-4">Password</label>
+					<div class="input-group mb-4">
+						<input type="password" class="form-control  p-2 ps-3 fs-4" placeholder="Enter password" name = "txtpassword" id = "txtpassword">
+						<button class="btn btn-outline-secondary px-4" type="button" id="toggle-hide-pass">
+							<i class="fa-solid fa-eye" id="visible-pass"></i>
+							<i class="fa-regular fa-eye-slash" id="invisible-pass"></i>
+						</button>
+					</div>
+					
+					<div class="d-flex justify-content-between mb-5">
+						<div class="d-flex">
+							<input type="checkbox" class="me-2" name="remember" id="remember" value="false">
+							<label for="remember" class="fs-4">Remember me</label>
+						</div>
+						<a href="#" class="fs-4">Forgot Password?</a>
+					</div>
+
+					<input type = "submit" class="btn bg-blue text-light fs-5 py-3 fw-bold" name = "btnlogin" value = "LOGIN">
+
+					<div class="d-flex mt-4 mb-2">
+						<hr class="w-100"> 
+						<p class="text-nowrap mx-2 fs-5">or login with</p> 
+						<hr class="w-100">
+					</div>	
+
+					<div class="d-flex justify-content-between other-login gap-3">
+						<div class="w-100 py-3 d-flex justify-content-center align-items-center btn border border-secondary">
+							<img src="./assets/img/google_logo.png" alt="" style="height: 15px;">
+							<p class="mb-0 ms-2 fs-4">Google</p>
+						</div>
+
+						<div class="w-100 py-3 d-flex justify-content-center align-items-center btn border border-secondary">
+							<img src="./assets/img/fb_logo.png" alt="" style="height: 15px;">
+							<p class="mb-0 ms-2 fs-4">Facebook</p>
+						</div>
+					</div>
+
+					<p class="redirect-signup fs-4 text-secondary text-center mt-4">Doesn't have an account yet? <a href="#">Sign Up</a></p>
+				</form>
+			</div>
+			<div class="img-con d-none d-lg-block col-4 px-0 bg-blue position-relative">
+				<div class="login-title px-4">
+					<p class="fs-1 mx-3 text-light text-center">Technical Support Management System</p>
 				</div>
-				<input type = "password" class="form-control p-2 ps-3 fs-4 mb-3" name = "txtpassword">
-				
-				<div class="d-flex mb-5">
-					<input type="checkbox" class="me-2" name="remember" id="remember" value="false">
-					<label for="remember" class="fs-4">Remember me</label>
-				</div>
-
-				<input type = "submit" class="btn bg-blue text-light fs-5 py-3 fw-bold" name = "btnlogin" value = "LOGIN">
-
-				<div class="d-flex my-2">
-					<hr class="w-100"> 
-					<p class="text-nowrap mx-2 pt-1 fs-6">or login with</p> 
-					<hr class="w-100">
+				<div class="img-wrapper" style="height:100%; width: 100%; overflow: hidden;">
+					<img src="./assets/img/Programmer.svg" alt="programmer.svg" style="margin-top:-1rem; scale: 1.1;">
 				</div>	
-
-				<div class="d-flex justify-content-between other-login gap-3">
-					<div class="w-100 py-3 d-flex justify-content-center align-items-center btn border border-secondary">
-						<img src="./assets/img/google_logo.png" alt="">
-						<p class="mb-0 ms-2 fs-4">Google</p>
-					</div>
-
-					<div class="w-100 py-3 d-flex justify-content-center align-items-center btn border border-secondary">
-						<img src="./assets/img/fb_logo.png" alt="">
-						<p class="mb-0 ms-2 fs-4">Facebook</p>
-					</div>
-				</div>
-
-				<p class="redirect-signup fs-4 text-secondary text-center mt-4">Doesn't have an account yet? <a href="#">Sign Up</a></p>
-			</form>
-		</div>
-		<div class=" col-3 img-con bg-cyan rounded-end">
-			<div class="img-wrapper">
-				<img src="./assets/img/Programmer.svg" alt="programmer.svg">
 			</div>
 		</div>
 	</div>
- 	
 </body>
+<script>
+	const toggle_hide_pass = document.getElementById("toggle-hide-pass");
+	const visible_pass = document.getElementById("visible-pass");
+	const invisible_pass = document.getElementById("invisible-pass");
+	const input_pass = document.getElementById("txtpassword");
+
+	toggle_hide_pass.addEventListener('click', () => {
+		if (invisible_pass.style.display == "block") {
+			visible_pass.style.display =  "block";
+			invisible_pass.style.display =  "none";
+			txtpassword.type = "password";
+		}
+		else {
+			visible_pass.style.display =  "none";
+			invisible_pass.style.display =  "block";
+			txtpassword.type = "text";
+		}
+	})
+</script>
 </html>
